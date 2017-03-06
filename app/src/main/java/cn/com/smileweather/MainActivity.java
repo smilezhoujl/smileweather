96817e378747693e5914a094dab45a3b104578c3
jackzhou <freelancerflyworld@gmail.com>
2017-03-06 23:49:33 +0800

    create db table and config

--- app/src/main/java/cn/com/smileweather/MainActivity.java
+++ app/src/main/java/cn/com/smileweather/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 /**
  * GitHub : https://github.com/smilezhoujl/smileweather.git
+ * weather api_key : aacd0e37ac5e41e9a431f0e819a44306
  **/
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/cn/com/smileweather/db/Province.java
@@ -0,0 +1,41 @@
+package cn.com.smileweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by jackzhou on 17/3/6.
+ */
+
+public class Province extends DataSupport{
+
+    private int id ;
+    private String provinceName ;
+    private int provinceCode ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+
+}
