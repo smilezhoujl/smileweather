9219ca8d25c2b1e27c703e5966739d9894e9b7fb
jackzhou <freelancerflyworld@gmail.com>
2017-03-07 22:54:08 +0800

    finished choise Provines City County function

---
+++ app/src/main/java/cn/com/smileweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package cn.com.smileweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by jackzhou on 17/3/7.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address , okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build() ;
+        client.newCall(request).enqueue(callback);
+    }
+}
